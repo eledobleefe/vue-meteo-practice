294d9ee83f0b455c249af6fce27c9e08a1a22ca9
eledobleefe <65417731+eledobleefe@users.noreply.github.com>
2021-02-05 22:18:38 +0100

    Actualización Alerta
    Añadidas opciones de color y de botón para volver a la home

--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -1,15 +1,25 @@
 <template>
-    <div class="align-center ma-5">
-        <v-alert color="blue-grey" dark dense icon="mdi-alert-circle" prominent>
+    <v-container class="align-center ma-10">
+        <v-alert :color="color" dark dense icon="mdi-alert-circle" prominent>
             <span  class="mr-4">{{mensaje}}</span>
         </v-alert>
-    </div>
+        <v-container v-if="volver" class="d-flex justify-center">
+            <v-btn :color="color" dark :href="volver">
+              Volver
+            <v-icon dark right>
+              mdi-arrow-left-circle
+            </v-icon>
+          </v-btn>
+        </v-container>
+    </v-container>
 </template>
 
 <script>
 export default {
     props: {
-        mensaje:{}
+        mensaje:{},
+        color: {},
+        volver: null
     }
 }
 </script>
