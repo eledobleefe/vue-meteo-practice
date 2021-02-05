0e867ac54e41e998697d610a53e59a9ebfd1e0e6
eledobleefe <65417731+eledobleefe@users.noreply.github.com>
2021-02-05 16:19:30 +0100

    Componente Alert
    Componente alert al que se le pasa el texto

---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="align-center ma-5">
+        <v-alert color="blue-grey" dark dense icon="mdi-alert-circle" prominent>
+            <span  class="mr-4">{{mensaje}}</span>
+        </v-alert>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        mensaje:{}
+    }
+}
+</script>
